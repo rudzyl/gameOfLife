aea7d2e79ba5117362a9026947a42473e2d46b4b
rugile <rugile.geceviciute@gmail.com>
2021-01-13 00:55:32 +0200

    Initial commit

---
+++ src/gameoflife/GameOfLife.java
@@ -0,0 +1,20 @@
+
+package gameoflife;
+
+
+public class GameOfLife {
+    
+    public static void main(String[] args) {
+        char [][] field = new char[50][50];
+        for (int i=0;i<field.length;i++) {
+            for(int j =0; j<field[i].length; j++) {
+                if(Math.random() > 0.22) {
+                    field[i][j] = 'X';
+                }else {
+                    field[i][j]='O';
+                }
+            }
+        }
+    }
+    
+}
